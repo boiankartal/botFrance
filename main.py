e82807c9f82efe19a3f255ce5d18f798b470edec
Bojan <boankartal@gmail.com>
2024-07-09 10:01:34 +0300

    добавил рассылку фото

--- main.py
+++ main.py
@@ -28,6 +28,11 @@ class Broadcast(StatesGroup):
     text = State()
 
 
+class Broadcast_photo(StatesGroup):
+    text = State()
+    media = State()
+
+
 @router.message(Command("broadcasts"))
 async def broadcast_start(message: Message, state: FSMContext):
     if message.from_user.id == int(os.getenv("admin_id_1")):
@@ -48,7 +53,50 @@ async def broadcast(message: Message, state: FSMContext):
         await message.answer("Рассылка началась", reply_markup=ReplyKeyboardRemove())
         for user in users:
             try:
-                await bot.send_message(chat_id=user.tg_id, text=f"{data["text"]}", parse_mode=ParseMode.MARKDOWN)
+                await bot.send_message(
+                    chat_id=user.tg_id,
+                    text=f"{data['text']}",
+                    parse_mode=ParseMode.MARKDOWN,
+                )
+            except:
+                continue
+
+
+######################################################################################
+
+
+@router.message(Command("broadcasts_photo"))
+async def broadcast_photo_start(message: Message, state: FSMContext):
+    if message.from_user.id == int(os.getenv("admin_id_1")):
+        await state.set_state(Broadcast_photo.media)
+        await message.answer("Пришли фото", reply_markup=kb.otmena)
+
+
+@router.message(Broadcast_photo.media)
+async def broadcast_photo_1(message: Message, state: FSMContext):
+    if message.text == "Сбросить":
+        await state.clear()
+        await message.answer("Рассылка отменена", reply_markup=ReplyKeyboardRemove())
+    else:
+        await state.update_data(media=message.photo[-1].file_id)
+        await state.set_state(Broadcast_photo.text)
+        await message.answer("Пришли подпись к фото", reply_markup=kb.otmena)
+
+
+@router.message(Broadcast_photo.text)
+async def broadcast_photo_2(message: Message, state: FSMContext):
+    if message.text == "Сбросить":
+        await state.clear()
+        await message.answer("Рассылка отменена", reply_markup=ReplyKeyboardRemove())
+    else:
+        await state.update_data(text=message.text)
+        data = await state.get_data()
+        users = await rq.get_users()
+        await state.clear()
+        await message.answer("Рассылка началась", reply_markup=ReplyKeyboardRemove())
+        for user in users:
+            try:
+                await bot.send_photo(chat_id=user.tg_id, photo=data["media"],  caption=f"{data["text"]}", parse_mode=ParseMode.MARKDOWN)
             except:
                 continue
 
